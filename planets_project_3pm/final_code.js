1fc658aad3410d44a89d237fbc2e79fa1774e415
Ryan Morales <ryan.morales@west-mec.org>
2024-08-19 16:38:43 -0700

    added hints for metric system

--- planets_project_3pm/final_code.js
+++ planets_project_3pm/final_code.js
@@ -4,11 +4,51 @@ const earthGravityFactors = require('./utils/earthGravityFactors.js');
 // import alienGravityFactors
 const alienGravityFactors = require('./utils/alienGravityFactors.js');
 
-function showUserFactors() {
+function showUserFactors(factorType, factorUnit, factorValue, factorPlanets) {
+    // final results object {}
+    // final calculations
+    // final console log to user
     getUserFactors();
- }
+}
 function getUserFactors() {
+    let factorType;
+    let factorValue;
+    let factorPlanets;
+    let factorSystem;
+    while (true) {
+        break;
+    }
+    while (true) {
+        if (factorType !== "pushups") {
+            // prompt
+            factorSystem = prompt("enter 1 or 2");
+        } else {
+            factorSystem = "3"
+        }
+        if (!isNaN(factorSystem) && (1000 > factorSystem > 0)) {
+            switch (factorSystem) {
+                case "1":
+                    factorSystem = "metric";
+                    break;
+                case "2":
+                    factorSystem = "imperial";
+                    break;
+            }
+            break;
+        } else {
+            console.error("you silly goose please enter a valid integer");
 
+        }
+    }
+    let factorUnit = "repetitions";
+    if (factorSystem === "metric") { } else if (factorSystem === "imperial") { }
+    while (true) {
+        break;
+    }
+    while (true) {
+        break;
+    }
+    showUserFactors(factorType, factorUnit, factorValue, factorPlanets);
 }
 global.getUserFactors = getUserFactors;
 getUserFactors();
